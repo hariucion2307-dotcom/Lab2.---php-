92ba7c39b287cd760e9cb023984bbc11bc085d83
hariucion2307-dotcom <hariucion2307@gmail.com>
2026-09-13 21:28:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Привет, мир!";
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+$days = 288;
+$message = "Все возвращаются на работу!";
+echo "$message" . $days . "<br />";
+echo "$days<br />";
+echo "$message<br />";
